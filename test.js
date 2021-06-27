1e44e0945749a22fc9782587a3d4b6ee3af7d532
Quang Cat <catvinhquang@gmail.com>
2021-06-27 17:43:57 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 try {
+    // 1. List site
     // alert('Ready for the simple life ?!');
     
     document.getElementsByTagName('header')?.[0]?.remove();
+    
     document.getElementById('primary-nav')?.remove();
+    // document.getElementById('primary-nav').style.visibility = 'hidden';
+
     // document.getElementById('notice')?.remove();
     document.getElementsByClassName('breadcrumb')?.[0]?.remove();
     document.getElementsByClassName('search')?.[0]?.remove();
@@ -13,6 +17,20 @@ try {
     const wrapper = document.getElementById('wrapper');
     wrapper.style.backgroundColor = '#000';
     wrapper.style.boxShadow = 'none';
+
+    // 2. Video site
+    document.getElementsByClassName('play-right')?.[0]?.remove();
+    document.getElementsByClassName('toolbar')?.[0]?.remove();
+    document.getElementsByClassName('video-info')?.[0]?.remove();
+    document.getElementsByClassName('video-content')?.[0]?.remove();
+    document.getElementsByClassName('box')?.[0]?.remove();
+
+    const player = document.getElementById('video');
+    if (player != null) {
+        player.style.borderRight = 'none';
+        player.style.width = '100%';
+        player.style.height = '100%';
+    }
 } catch (e) {
     alert(e);
 }
