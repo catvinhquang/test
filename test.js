16462bf8eca3af8735fdd1c9387fe0c9f485e05e
Quang Cat <catvinhquang@gmail.com>
2021-06-27 16:18:28 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-alert("Ready for the simple life ?!)");
-document.body.style.background = "#fff";
+alert('Ready for the simple life ?!');
+document.body.style.background = "#000";
 document.getElementsByTagName('header')?.[0]?.remove();
 document.getElementById('primary-nav')?.remove();
-document.getElementById('notice')?.remove();
+// document.getElementById('notice')?.remove();
+document.getElementsByClassName('breadcrumb')?.[0]?.remove();
+document.getElementsByClassName('search')?.[0]?.remove();
+document.getElementsByTagName('footer')?.[0]?.remove();
