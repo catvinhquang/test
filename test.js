3228b4ae7bc5dd6a058e6bf16f69a7e9a18ce595
Quang Cat <catvinhquang@gmail.com>
2021-06-27 16:46:34 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,18 @@
-alert('Ready for the simple life ?!');
-document.body.style.background = "#000";
-document.getElementsByTagName('header')?.[0]?.remove();
-document.getElementById('primary-nav')?.remove();
-// document.getElementById('notice')?.remove();
-document.getElementsByClassName('breadcrumb')?.[0]?.remove();
-document.getElementsByClassName('search')?.[0]?.remove();
-document.getElementsByTagName('footer')?.[0]?.remove();
+try {
+    alert('Ready for the simple life ?!');
+    
+    document.getElementsByTagName('header')?.[0]?.remove();
+    document.getElementById('primary-nav')?.remove();
+    // document.getElementById('notice')?.remove();
+    document.getElementsByClassName('breadcrumb')?.[0]?.remove();
+    document.getElementsByClassName('search')?.[0]?.remove();
+    document.getElementsByTagName('footer')?.[0]?.remove();
+
+    document.body.style.backgroundColor = '#000';
+
+    const wrapper = document.getElementById('wrapper');
+    wrapper.style.backgroundColor = '#000';
+    wrapper.style.boxShadow = 'none';
+} catch (e) {
+    alert(e);
+}
