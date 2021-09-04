7ad36e1445d5cd4d675d3121a92e272288252454
Quang Cat <catvinhquang@gmail.com>
2021-09-04 18:16:26 +0700

    Update finance.yahoo.com.js

--- finance.yahoo.com.js
+++ finance.yahoo.com.js
@@ -2,3 +2,4 @@ console.log('Hello world!');
 document.getElementById('mrt-node-Lead-0-Ad').remove();
 document.getElementById('mrt-node-Col2-0-ComboAd').remove();
 document.getElementById('mrt-node-Col2-1-QuoteModule').remove();
+document.getElementById('mrt-node-Col2-14-AdUnitWithTdAds').remove();
