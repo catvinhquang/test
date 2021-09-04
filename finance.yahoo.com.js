74cb63ab44fc3015c05b2b6d862a5f2e0fb1ea8e
Quang Cat <catvinhquang@gmail.com>
2021-09-04 23:25:47 +0700

    Update finance.yahoo.com.js

--- finance.yahoo.com.js
+++ finance.yahoo.com.js
@@ -1,11 +1,11 @@
 console.log('Hello world!');
 
 // home
-document.getElementById('mrt-node-Lead-1-Ad').remove();
+document.getElementById('mrt-node-Lead-1-Ad')?.remove();
 
 // /quote/*
-document.getElementById('mrt-node-Lead-0-Ad').remove();
-document.getElementById('mrt-node-Col2-0-ComboAd').remove();
-document.getElementById('mrt-node-Col2-1-QuoteModule').remove();
-document.getElementById('mrt-node-Col2-14-AdUnitWithTdAds').remove();
-document.getElementById('mrt-node-Col2-15-LinkOut').remove();
+document.getElementById('mrt-node-Lead-0-Ad')?.remove();
+document.getElementById('mrt-node-Col2-0-ComboAd')?.remove();
+document.getElementById('mrt-node-Col2-1-QuoteModule')?.remove();
+document.getElementById('mrt-node-Col2-14-AdUnitWithTdAds')?.remove();
+document.getElementById('mrt-node-Col2-15-LinkOut')?.remove();
