06968c24a33bfb07fba76ea9a3251992c703c80a
Quang Cat <catvinhquang@gmail.com>
2021-09-04 23:33:00 +0700

    Update finance.yahoo.com.js

--- finance.yahoo.com.js
+++ finance.yahoo.com.js
@@ -1,11 +1,16 @@
-console.log('Hello world!');
+try {
+  // home
+  document.getElementById('mrt-node-Lead-1-Ad')?.remove();
 
-// home
-document.getElementById('mrt-node-Lead-1-Ad')?.remove();
-
-// /quote/*
-document.getElementById('mrt-node-Lead-0-Ad')?.remove();
-document.getElementById('mrt-node-Col2-0-ComboAd')?.remove();
-document.getElementById('mrt-node-Col2-1-QuoteModule')?.remove();
-document.getElementById('mrt-node-Col2-14-AdUnitWithTdAds')?.remove();
-document.getElementById('mrt-node-Col2-15-LinkOut')?.remove();
+  // /quote/*
+  document.getElementById('mrt-node-Lead-0-Ad')?.remove();
+  document.getElementById('mrt-node-Col2-0-ComboAd')?.remove();
+  document.getElementById('mrt-node-Col2-1-QuoteModule')?.remove();
+  document.getElementById('mrt-node-Col2-14-AdUnitWithTdAds')?.remove();
+  document.getElementById('mrt-node-Col2-15-LinkOut')?.remove();
+  
+  console.log('Apply successful');
+} catch(e) {
+  console.log('Apply failed');
+  console.log(e);
+}
