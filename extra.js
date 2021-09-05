73898b06545e067a8883aee970ebb62c5f148a0b
Quang Cat <catvinhquang@gmail.com>
2021-09-05 18:19:51 +0700

    Update extra.js

--- extra.js
+++ extra.js
@@ -1,12 +1,8 @@
 fetch('hi-curious', {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'no-cors', // cors, *cors, same-origin
-    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    // credentials: 'same-origin', // include, *same-origin, omit
+    method: 'POST',
+    mode: 'no-cors',
     headers: {
         'Content-Type': 'application/json'
     },
-    // redirect: 'follow', // manual, *follow, error
-    // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: '{"text": "Hello, this is some text\nThis is more text. :tada:"}'
 });
