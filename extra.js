b06dc3f9298273a8c608cfed6b56631c29131eb9
Quang Cat <catvinhquang@gmail.com>
2021-09-10 09:43:36 +0700

    Delete extra.js

--- extra.js
+++
@@ -1,8 +0,0 @@
-fetch('hi-curious', {
-    method: 'POST',
-    mode: 'no-cors',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: '{"text": "Hello, this is some text\nThis is more text. :tada:"}'
-});
